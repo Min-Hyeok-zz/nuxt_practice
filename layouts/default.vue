5691953292b469a0e3e469f5c1166712bdd18825
PPentech <alsgur0008@naver.com>
2018-06-07 23:49:14 +0900

    page test

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,27 @@
 <template>
   <div>
+    <header id="header">
+      <div class="logo"><app-logo/></div>
+      <ul id="gnb">
+        <li><a href="/member/login" class="button--green">로그인</a></li>
+        <li><a href="/member/add" class="button--green">회원가입</a></li>
+        <li><a href="/" class="button--green">메인</a></li>
+        <li><a href="/board/list" class="button--green">게시판</a></li>
+        <li><a href="#!" class="button--green">로그아웃</a></li>
+      </ul>
+    </header>
     <nuxt/>
   </div>
 </template>
+<script>
+import AppLogo from '~/components/AppLogo.vue'
 
+export default {
+  components: {
+    AppLogo
+  }
+}
+</script>
 <style>
 html {
   font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
@@ -19,6 +37,22 @@ html {
 *, *:before, *:after {
   box-sizing: border-box;
   margin: 0;
+  list-style: none;
+}
+
+#header{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  max-width: 1000px;
+  margin: 0 auto;
+  margin-top: 20px;
+}
+#gnb{
+  display: flex;
+}
+#gnb > li{
+  margin: 0 5px;
 }
 
 .button--green {
@@ -28,11 +62,12 @@ html {
   color: #3b8070;
   text-decoration: none;
   padding: 10px 30px;
+  transition: .4s;
 }
 
 .button--green:hover {
   color: #fff;
-  background-color: #3b8070;
+  background-color: #3b8070;transition: .4s;
 }
 
 .button--grey {
@@ -48,5 +83,38 @@ html {
 .button--grey:hover {
   color: #fff;
   background-color: #35495e;
+}
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
+  display: block;
+  font-weight: 300;
+  font-size: 50px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 24px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+.form-control{
+  padding: 12px 10px;
+  color: #35495e;
+  border-radius: 5px;
+  border:1px solid #bebebe;
 }
 </style>
--- pages/board/list.vue
+++ pages/board/list.vue
@@ -1,7 +1,6 @@
 <template>
   <section class="container">
     <div>
-      <app-logo/>
       <h1 class="title">
         게시판
       </h1>
@@ -9,7 +8,6 @@
         게시글 리스트
       </h2>
       <div class="links">
-        <a href="/" class="button--green">메인으로</a>
       </div>
     </div>
   </section>
@@ -26,32 +24,5 @@ export default {
 </script>
 
 <style>
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
 
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
 </style>
